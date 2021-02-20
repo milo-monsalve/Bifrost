86d5e28587232cc2f58701b52a55650cc92ae6aa
camilo mejia monsalve <camilo.mon96@gmail.com>
2021-02-19 21:31:41 -0500

    Adding constructors to SqlServerProcesses and MySqlProcesses to set the project configuration at time to instance the classes.

--- Bifrost/SQL/Processes/MySql.cs
+++ Bifrost/SQL/Processes/MySql.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using MySql.Data.MySqlClient;
 using Bifrost.SQL.Connections;
 using Bifrost.SQL.Processes;
+using Microsoft.Extensions.Configuration;
 
 
 namespace Bifrost.Processes
 {
     public class MySqlProcesses
     {
+        private readonly IConfiguration config;
+        public MySqlProcesses(IConfiguration config)
+        {
+            this.config = config;
+        }
+
         public static List<T> SpToList<T>(string app, string storedProcedure, MySqlParameter parameter)
         {
             using (MySqlConnection connection = BifrostMySql.Get(app))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return Convert.ReaderToList<T>(command.ExecuteReader());
+            }
+        }
+
+        public List<T> SpToList<T>(string storedProcedure, MySqlParameter parameter, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
             using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
@@ -32,9 +50,32 @@ namespace Bifrost.Processes
             }
         }
 
+        public List<T> SpToList<T>(string storedProcedure, MySqlParameter[] parameters, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (MySqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return Convert.ReaderToList<T>(command.ExecuteReader());
+            }
+        }
+
         public static string SpToStringScalar(string app, string storedProcedure, MySqlParameter parameter)
         {
             using (MySqlConnection connection = BifrostMySql.Get(app))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return command.ExecuteScalar().ToString();
+            }
+        }
+
+        public string SpToStringScalar(string storedProcedure, MySqlParameter parameter, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
             using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
@@ -55,9 +96,32 @@ namespace Bifrost.Processes
             }
         }
 
+        public string SpToStringScalar(string storedProcedure, MySqlParameter[] parameters, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (MySqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return command.ExecuteScalar().ToString();
+            }
+        }
+
         public static int SpToIntScalar(string app, string storedProcedure, MySqlParameter parameter)
         {
             using (MySqlConnection connection = BifrostMySql.Get(app))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+
+        public int SpToIntScalar(string app, string storedProcedure, MySqlParameter parameter, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
             using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
@@ -78,9 +142,32 @@ namespace Bifrost.Processes
             }
         }
 
+        public int SpToIntScalar(string app, string storedProcedure, MySqlParameter[] parameters, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (MySqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+
         public static double SpToDoubleScalar(string app, string storedProcedure, MySqlParameter parameter)
         {
             using (MySqlConnection connection = BifrostMySql.Get(app))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToDouble(command.ExecuteScalar());
+            }
+        }
+
+        public double SpToDoubleScalar(string storedProcedure, MySqlParameter parameter, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
             using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
@@ -101,6 +188,18 @@ namespace Bifrost.Processes
             }
         }
 
+        public double SpToDoubleScalar(string storedProcedure, MySqlParameter[] parameters, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (MySqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToDouble(command.ExecuteScalar());
+            }
+        }
+
         public static bool SpToBoolScalar(string app, string storedProcedure, MySqlParameter parameter)
         {
             using (MySqlConnection connection = BifrostMySql.Get(app))
@@ -112,9 +211,32 @@ namespace Bifrost.Processes
             }
         }
 
+        public bool SpToBoolScalar(string storedProcedure, MySqlParameter parameter, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToBoolean(command.ExecuteScalar());
+            }
+        }
+
         public static bool SpToBoolScalar(string app, string storedProcedure, MySqlParameter[] parameters)
         {
             using (MySqlConnection connection = BifrostMySql.Get(app))
+            using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (MySqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToBoolean(command.ExecuteScalar());
+            }
+        }
+
+        public bool SpToBoolScalar(string storedProcedure, MySqlParameter[] parameters, string setting = "app")
+        {
+            using (MySqlConnection connection = BifrostMySql.Get(config[setting]))
             using (MySqlCommand command = new MySqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
--- Bifrost/SQL/Processes/SqlServer.cs
+++ Bifrost/SQL/Processes/SqlServer.cs
@@ -3,14 +3,31 @@ using System.Data.SqlClient;
 using System.Collections.Generic;
 using Bifrost.SQL.Connections;
 using Bifrost.SQL.Processes;
+using Microsoft.Extensions.Configuration;
 
 namespace Bifrost
 {
     public class SqlServerProcesses
     {
+        private readonly IConfiguration config;
+        public SqlServerProcesses(IConfiguration config)
+        {
+            this.config = config;
+        }
         public static List<T> SpToList<T>(string app, string storedProcedure, SqlParameter parameter)
         {
             using (SqlConnection connection = BifrostSqlServer.Get(app))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return Convert.ReaderToList<T>(command.ExecuteReader());
+            }
+        }
+
+        public List<T> SpToList<T>(string app, string storedProcedure, SqlParameter parameter, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
             using (SqlCommand command = new SqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
@@ -31,9 +48,32 @@ namespace Bifrost
             }
         }
 
+        public List<T> SpToList<T>(string storedProcedure, SqlParameter[] parameters, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (SqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return Convert.ReaderToList<T>(command.ExecuteReader());
+            }
+        }
+
         public static string SpToStringScalar(string app, string storedProcedure, SqlParameter parameter)
         {
             using (SqlConnection connection = BifrostSqlServer.Get(app))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return command.ExecuteScalar().ToString();
+            }
+        }
+
+        public string SpToStringScalar(string storedProcedure, SqlParameter parameter, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
             using (SqlCommand command = new SqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
@@ -54,9 +94,32 @@ namespace Bifrost
             }
         }
 
+        public string SpToStringScalar(string storedProcedure, SqlParameter[] parameters, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (SqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return command.ExecuteScalar().ToString();
+            }
+        }
+
         public static int SpToIntScalar(string app, string storedProcedure, SqlParameter parameter)
         {
             using (SqlConnection connection = BifrostSqlServer.Get(app))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+
+        public int SpToIntScalar(string storedProcedure, SqlParameter parameter, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
             using (SqlCommand command = new SqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
@@ -77,9 +140,32 @@ namespace Bifrost
             }
         }
 
+        public int SpToIntScalar(string storedProcedure, SqlParameter[] parameters, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (SqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToInt32(command.ExecuteScalar());
+            }
+        }
+
         public static double SpToDoubleScalar(string app, string storedProcedure, SqlParameter parameter)
         {
             using (SqlConnection connection = BifrostSqlServer.Get(app))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToDouble(command.ExecuteScalar());
+            }
+        }
+
+        public double SpToDoubleScalar(string storedProcedure, SqlParameter parameter, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
             using (SqlCommand command = new SqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
@@ -100,6 +186,18 @@ namespace Bifrost
             }
         }
 
+        public double SpToDoubleScalar(string storedProcedure, SqlParameter[] parameters, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (SqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToDouble(command.ExecuteScalar());
+            }
+        }
+
         public static bool SpToBoolScalar(string app, string storedProcedure, SqlParameter parameter)
         {
             using (SqlConnection connection = BifrostSqlServer.Get(app))
@@ -111,9 +209,32 @@ namespace Bifrost
             }
         }
 
+        public bool SpToBoolScalar(string storedProcedure, SqlParameter parameter, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToBoolean(command.ExecuteScalar());
+            }
+        }
+
         public static bool SpToBoolScalar(string app, string storedProcedure, SqlParameter[] parameters)
         {
             using (SqlConnection connection = BifrostSqlServer.Get(app))
+            using (SqlCommand command = new SqlCommand(storedProcedure, connection))
+            {
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+                foreach (SqlParameter parameter in parameters)
+                    command.Parameters.Add(parameter).Value = parameter.Value;
+                return System.Convert.ToBoolean(command.ExecuteScalar());
+            }
+        }
+
+        public bool SpToBoolScalar(string app, string storedProcedure, SqlParameter[] parameters, string setting = "app")
+        {
+            using (SqlConnection connection = BifrostSqlServer.Get(config[setting]))
             using (SqlCommand command = new SqlCommand(storedProcedure, connection))
             {
                 command.CommandType = System.Data.CommandType.StoredProcedure;
